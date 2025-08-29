8c88a403432614310b65b75979218075e3db7073
harkl <3zy3@protonmail.ch>
2025-08-29 15:38:20 +1200

    setup nav transaltions

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,29 +1,29 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "@radix-ui/react-navigation-menu";
+import { useTranslations } from "next-intl";
 
 export function Nav() {
+  const t = useTranslations("Nav");
   return (
     <NavigationMenu className="w-full flex justify-center bg-white shadow-md py-1 mb-4">
       <NavigationMenuList className="flex space-x-6">
         <NavigationMenuItem>
           <NavigationMenuLink className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md transition-colors">
-            Link 1
+            {t("link1")}
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md transition-colors">
-            Link 2
+            {t("link2")}
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md transition-colors">
-            Link 3
+            {t("link3")}
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
